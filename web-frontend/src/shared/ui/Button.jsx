e05fead68ba61017bd603c5ebd120efe6586bae8
seonghun120614 <seonghun120614@gmail.com>
2025-02-21 19:48:24 +0900

    CHORE: Re-construct file structure

--- web-frontend/src/shared/ui/Button.jsx
+++ web-frontend/src/shared/ui/Button.jsx
@@ -1,2 +1,2 @@
 // 버튼은 색깔도 유동적으로 만들 수 있음, ex. 취소, 확인 버튼이 명확히 나뉘어짐
-// Input: name(버튼 이름), variant(버튼 색깔), func(누르면 실행), img(optional 있으면 먼저 띄워지고 그 밑에 이름), onChange
+// Input: name(버튼 이름), variant(버튼 색깔), func(누르면 실행), img(optional 있으면 먼저 띄워지고 그 밑에 이름)
--- web-frontend/src/shared/ui/Input.jsx
+++ web-frontend/src/shared/ui/Input.jsx
@@ -1,2 +1,3 @@
 // 인풋 밑에는 빨간글 설명이 들어가는 공간이 필요 시 들어갈 수 있음
-// Input: title(제목 - input box 위에 띄워질 것), desc(설명 - placeholder 랑 동일), helperTxt(밑에 빨간 글씨로 띄워짐)
+// Input: title(제목 - input box 위에 띄워질 것), desc(설명 - placeholder 랑 동일),
+//          func(인수로 input box 안의 들어가는 값이 change 할 때마다 받아짐, return은 string으로 빨간 글씨로 띄워짐)
